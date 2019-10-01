ec8c3c78cbe491dadcf5a81c35126647ae5538c8
Carlos Martinez <cmartinez0818@eagle.fgcu.edu>
2019-10-01 15:01:47 -0400

    controller waiting on DB

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,71 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class Controller {
+  @FXML
+  private Button loginButton;
+
+  @FXML
+  private TextField userField;
+
+  @FXML
+  private TextField passwordField;
+
+  @FXML
+  private Button registerButton;
+
+  @FXML
+  void loginButton() {
+
+
+  }
+
+  public void initialize() {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/CompetitionDatabase";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+
+      String sql = "SELECT * FROM JOBS";
+
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+      }
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+  }
 }
