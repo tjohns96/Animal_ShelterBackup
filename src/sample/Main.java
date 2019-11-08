20dad5d2f003f931ae0c4419086fe84250fe752b
Michael Gough <Magough3199@eagle.fgcu.edu>
2019-11-08 10:58:26 -0500

    Additional Gui

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,22 @@
 package sample;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.swing.Action;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        primaryStage.setTitle("Animal Shelter Manager");
+        primaryStage.setTitle("Animal Shelter");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
     }
 
 
