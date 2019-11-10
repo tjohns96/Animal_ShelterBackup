1d63653c506ca13165c5122d8d7fb5fc02a5b36f
Michael Gough <Magough3199@eagle.fgcu.edu>
2019-11-09 20:27:32 -0500

    Animal Information FXML

--- src/sample/HomePageController.java
+++ src/sample/HomePageController.java
@@ -1,21 +1,54 @@
 package sample;
 
-import java.awt.event.ActionEvent;
+import java.awt.Button;
+import java.awt.MenuItem;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuItem;
-import java.sql.ResultSet;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
 
 public class HomePageController {
+
+
+  public
+  javafx.scene.control.MenuItem menuItem_AdoptableDogs;
+
+  @FXML
+  private ListView<String> listView_Animals = new ListView<>();
+
+  public void loadScene(MenuItem pressedMenuItem, String nameOfFxml, String titleOftheScene) {
+
+    try {
+
+      Stage stage;
+
+      // retrieves and closes current stage
+      stage = (Stage) pressedMenuItem.getScene().getWindow();
+      stage.close();
+
+      // loads main screen stage
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AdoptableDogs.fxml"));
+      Parent profile = (Parent) fxmlLoader.load();
+
+      // creates a new stage
+      Stage newStage = new Stage();
+      newStage.setTitle("Dogs Up For Adoption");
+      newStage.setScene(new Scene(profile));
+
+      // set new stage to current stage and display stage
+      stage = newStage;
+      stage.show();
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+
+  }
 
   @FXML
   public void handleAdoptableDogsMenuItem(javafx.event.ActionEvent actionEvent) {
@@ -23,5 +56,16 @@ public class HomePageController {
     System.out.println("You would like to adopt a dog!!");
 
   }
+
+
+  public void initialize() {
+    listView_Animals.getItems().add("\t" + "EVERY PET DESERVES A FOREVER HOME");
+    listView_Animals.getItems().add("Our daily mission: ");
+    listView_Animals.getItems().add("° Arrive bright eyed and bushy tailed");
+    listView_Animals.getItems().add("° Insure every pet feels at home");
+    listView_Animals.getItems().add("° Keep animals and area clean");
+
+  }
+
 }
 
